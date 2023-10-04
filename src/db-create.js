217b13b79b4883e15af1b7eff85688d85bb1de6d
stramosk98 <antoniostramosk_@hotmail.com>
2023-10-04 18:46:58 -0300

    update

--- src/db-create.js
+++ src/db-create.js
@@ -1,12 +1,4 @@
-const { Pool } = require('pg');
-
-const pool = new Pool({
-    host: 'db',
-    port: 5432,
-    user: 'user123',
-    password: '1234',
-    database: 'projeto-hoteis'
-});
+const pool = require('./db')
 
 async function createTable() {
     try {
